2684522df3bd7929988ebe4126efd512b148b626
benitex-dev <benitezjorgedev@gmail.com>
2026-02-08 16:04:18 -0300

    creamos listado de Roles, autoridades y usuarios para realizar pruebas

--- src/main/java/com/plataforma_educativa/admnistrador_usuarios/security/config/SecurityConfig.java
+++ src/main/java/com/plataforma_educativa/admnistrador_usuarios/security/config/SecurityConfig.java
@@ -9,7 +9,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +43,37 @@ public class SecurityConfig {
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(null);
-        provider.setUserDetailsService(null);
+        provider.setUserDetailsService(userDetailsService());
         return provider;
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        List userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("admin")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("ADMINISTRADO")
+                .authorities("CREATE", "READ", "UPDATE", "DELETE")
+                .build());
+
+        userDetailsList.add(User.withUsername("alumno")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("ESTUDIANTE")
+                .authorities("READ","UPDATE")
+                .build());
+
+        userDetailsList.add(User.withUsername("profe")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("PROFESOR")
+                .authorities("UPDATE","READ","CREATE")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
 }
